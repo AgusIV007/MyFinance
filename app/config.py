77958971f34c794d574bc52ddfdf39f68586b1ad
AgusIV007 <aiturriavallejos@alumno.huergo.edu.ar>
2024-07-31 19:21:55 -0300

    CHAU

--- app/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://user:@localhost:3306/myfinance'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'
-    
-    @staticmethod
-    def print_db_uri():
-        print(Config.SQLALCHEMY_DATABASE_URI)
