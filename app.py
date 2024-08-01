973cfcac2a0c64852e7db7751300895f92b08b3d
AgusIV007 <aiturriavallejos@alumno.huergo.edu.ar>
2024-07-31 21:21:31 -0300

    Finalización de base de datos

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template, json, request
 import mysql.connector
-from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'i2i0L2aH1'
-app.config['MYSQL_DATABASE_DB'] = 'mydb'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
-
-conn = mysql.connect()
-cursor = conn.cursor()
-
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="i2i0L2aH1",
+    database="mydb"
+)
 
 @app.route("/")
 def main():
@@ -29,13 +25,20 @@ def showSignUp():
 def signUp():
     _name = request.form['inputName']
     _email = request.form['inputEmail']
-    _password = generate_password_hash(request.form['inputPassword'])
+    _password = request.form['inputPassword']
     # validate the received values 
-    cursor.callproc('finance_createUser',(_name,_email,_password))
     if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
+        try:
+            with conn.cursor() as cursor:
+                cursor.callproc('finance_createUser', (_name, _email, _password))
+            conn.commit()
+            return json.dumps({'html': '<span>All fields good !!</span>'})
+        except mysql.connector.Error as err:
+            conn.rollback()
+            return json.dumps({'html': f'<span>Error: {err}</span>'})
     else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
+
 
 if __name__ == "__main__":
     app.run()
